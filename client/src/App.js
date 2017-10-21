889ab94087bef1a5e66fef501af483521e922a05
Rohan Sapre <sapre.r@husky.neu.edu>
2017-10-21 19:08:15 -0400

    updated react app with latest changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,73 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  state = {users: []}
+class ConvenePlan extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
+  handleClick() {
+    this.props.onButtonClick('Hello World');
   }
 
   render() {
     return (
+      <div className="row">
+        <button type="button" className="btn btn-info btn-lg" onClick={this.handleClick}>
+          {this.props.data}
+        </button>
+      </div>
+    );
+  }
+}
+
+class EventPlanner extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {data: '', option: 'Convene'};
+  }
+
+  handleClick(value) {
+    this.setState({data: value});
+    console.log(value);
+  }
+
+  render() {
+    let answer = null;
+    if (this.state.data === '') {
+      answer = (
+        <div className="container-fluid">
+          <ConvenePlan data={this.state.option} onButtonClick={this.handleClick} />
+          <div className="row">
+            <button type="button" className="btn btn-primary btn-lg">Party</button>
+          </div>
+        </div>
+      );
+    } else {
+      answer = <h3>Hello World</h3>;
+    }
+    return (
+      <div className="container">
+        <div className="row">
+          <h1 className="App-title">Pick your poison!</h1>
+        </div>
+        {answer}
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
       <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to gather.now</h1>
+        </header>
+        <EventPlanner />
       </div>
     );
   }
