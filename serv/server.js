6067089abf71ddcdfad7520fca0582a1c86e8311
Bharat V <vaidhyanathan.b@husky.neu.edu>
2017-10-21 19:44:07 -0400

    Node server restructured

---
+++ serv/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public'));
+
+require ("./setup/app.js")(app);
+
+require ("./server/app.js")(app);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
